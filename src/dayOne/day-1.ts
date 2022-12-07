24a61ef7a805f720254f18d1ebb39ef4cb2ba58f
Cedric Robert <robert.ced@gmail.com>
2022-12-07 20:36:51 +0100

    resolve dayOne

--- src/dayOne/day-1.ts
+++ src/dayOne/day-1.ts
@@ -7,9 +7,23 @@ export async function dayOne() {
     const data = await fsPromises.readFile(path.join(__dirname, './input-1-1'), { encoding: 'utf-8' });
     const lines = data.split(/\r?\n/);
 
+    const maxCalories: number[] = [];
+    let maxCurrentGroup = 0;
+
     lines.forEach((line) => {
-      console.log(`line: ${line}`);
+      /*   console.log(`line: ${line}`); */
+      const value = Number.parseInt(line);
+      if (Number.isNaN(value)) {
+        // new group begin
+        maxCalories.push(maxCurrentGroup);
+        maxCurrentGroup = 0;
+      } else {
+        maxCurrentGroup += value;
+      }
     });
+
+    maxCalories.sort((a, b) => a - b).reverse();
+    console.log(maxCalories[0] + maxCalories[1] + maxCalories[2]);
   } catch (error) {
     console.log(error);
   }
