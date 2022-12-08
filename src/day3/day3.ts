990ecf00b9ea22fb85830e0f4c8ab5cb5b8305b6
Cedric Robert <robert.ced@gmail.com>
2022-12-08 21:36:49 +0100

    wip day3

--- src/day3/day3.ts
+++ src/day3/day3.ts
@@ -7,10 +7,14 @@ export async function day3() {
     const data = await fsPromises.readFile(path.join(__dirname, './sample'), { encoding: 'utf-8' });
     const lines = data.split(/\r?\n/);
 
-    let currentLineIndex = 1;
+    const currentLineIndex = 1;
 
     lines.forEach((line) => {
-      console.log(`line ${currentLineIndex++}/${lines.length}: ${line}`);
+      /* console.log(`line ${currentLineIndex++}/${lines.length}: ${line}`); */
+      const first = Array.from(line.slice(0, line.length / 2));
+      const second = Array.from(line.slice(line.length / 2));
+      const common = first.filter((charFirst) => second.find((charSecond) => charSecond === charFirst))[0];
+      console.log(`first ${first}  second ${second}  common ${common}`);
     });
   } catch (error) {
     console.log(error);
